6ef051192bd2b95858a553f0012a241542f299fc
dai-home pc <nbdavied@gmail.com>
2018-10-26 22:05:38 +0800

    命令执行顺序控制，资源锁
    两种情况会导致命令执行错误：
    1. 请求1在请求2之前发出，但服务端接收后先执行请求2
    2. 服务端接收到两条指令，通过串口发送至 arduino 时发送顺序错误，导致 arduino 无法正常解析，例如两条指令 `f,50,50;s; ` 顺序错误变成 `f,s;50,50;`
    程序通过 lock 和 seq  来确保资源的独占性和请求的顺序执行

--- raspiberry/car-server.py
+++ raspiberry/car-server.py
@@ -6,55 +6,132 @@ import time
 
 arduino = Serial('/dev/ttyAMA0', 9600, timeout=1)
 app = Flask(__name__)
-
+# 命令执行顺序控制变量，只有大于当前值的命令可以执行
+lastseq = 0
+# 资源锁，不等于0时表示上锁，无法被其他线程获取，直到释放
+lock = 0
 @app.route('/')
 def index():
-    return render_template('index.html') 
+    return render_template('index.html', seq=lastseq) 
 
-@app.route('/controller')
-def controller2():
-    return render_template('controller.html')
 
 @app.route('/forward')
 def forward():
+    global lastseq
     left = request.args.get('left', '255')
     right = request.args.get('right', '255')
-    arduino.write(b'f,')
-    arduino.write(bytes(left +"," + right, 'ascii'))
-    arduino.write(b';')
-    return 'forward, %s, %s' % (left, right)
+    seq = int(request.args.get('seq'))
+    # 获取往串口写数据的资源权限
+    getLock()
+    try:
+        # 控制命令执行顺序，避免异步操作顺序异常
+        if seq <= lastseq:
+            return 'seq error'
+        lastseq = seq
+        arduino.write(b'f,')
+        arduino.write(bytes(left +"," + right, 'ascii'))
+        arduino.write(b';')
+        return 'forward, %s, %s' % (left, right)
+    finally:
+        # 释放串口资源
+        releaseLock()
 
 @app.route('/brake')
 def brake():
-    arduino.write(b's;')
-    return 'brake'
+    global lastseq
+    seq = int(request.args.get('seq'))
+    # 获取往串口写数据的资源权限
+    getLock()
+    try:
+        # 控制命令执行顺序，避免异步操作顺序异常
+        if seq <= lastseq:
+            return 'seq error'
+        lastseq = seq
+
+        arduino.write(b's;')
+        return 'brake'
+    finally:
+        # 释放串口资源
+        releaseLock()
 
 @app.route('/backward')
 def backward():
+    global lastseq
     left = request.args.get('left', '255')
     right = request.args.get('right', '255')
-    arduino.write(b'b,')
-    arduino.write(bytes(left +"," + right, 'ascii'))
-    arduino.write(b';')
-    return 'backward, %s, %s' % (left, right)
+    seq = int(request.args.get('seq'))
+    # 获取往串口写数据的资源权限
+    getLock()
+    try:
+        # 控制命令执行顺序，避免异步操作顺序异常
+        if seq <= lastseq:
+            return 'seq error'
+        lastseq = seq
+        arduino.write(b'b,')
+        arduino.write(bytes(left +"," + right, 'ascii'))
+        arduino.write(b';')
+        return 'backward, %s, %s' % (left, right)
+    finally:
+        # 释放串口资源
+        releaseLock()
 
 @app.route('/right')
 def turnRight():
+    global lastseq
     left = request.args.get('left', '255')
     right = request.args.get('right', '255')
-    arduino.write(b'r,')
-    arduino.write(bytes(left +"," + right, 'ascii'))
-    arduino.write(b';')
-    return 'turn right, %s, %s' % (left, right)
+    seq = int(request.args.get('seq'))
+    # 获取往串口写数据的资源权限
+    getLock()
+    try:
+        # 控制命令执行顺序，避免异步操作顺序异常
+        if seq <= lastseq:
+            return 'seq error'
+        lastseq = seq
+        arduino.write(b'r,')
+        arduino.write(bytes(left +"," + right, 'ascii'))
+        arduino.write(b';')
+        return 'turn right, %s, %s' % (left, right)
+    finally:
+        # 释放串口资源
+        releaseLock()
 
 @app.route('/left')
 def turnLeft():
+    global lastseq
     left = request.args.get('left', '255')
     right = request.args.get('right', '255')
-    arduino.write(b'l,')
-    arduino.write(bytes(left +"," + right, 'ascii'))
-    arduino.write(b';')
-    return 'turn left, %s, %s' % (left, right)
+    seq = int(request.args.get('seq'))
+    # 获取往串口写数据的资源权限
+    getLock()
+    try:
+        # 控制命令执行顺序，避免异步操作顺序异常
+        if seq <= lastseq:
+            return 'seq error'
+        lastseq = seq
+        arduino.write(b'l,')
+        arduino.write(bytes(left +"," + right, 'ascii'))
+        arduino.write(b';')
+        return 'turn left, %s, %s' % (left, right)
+    finally:
+        # 释放串口资源
+        releaseLock()
+
+def getLock():
+    '''
+    获取写入串行数据资源
+    '''
+    global lock
+    while lock != 0:
+        sleep(10)
+    lock = 1
+
+def releaseLock():
+    '''
+    释放串行数据资源
+    '''
+    global lock
+    lock = 0
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True, port=8080) 
